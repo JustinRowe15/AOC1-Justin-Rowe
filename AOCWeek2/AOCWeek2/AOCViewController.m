b030575418e9aeb38ba576a9d60f084dce4e584d
JustinRowe15 <JustinRowe15@fullsail.edu>
2012-11-29 18:42:23 -0500

    Initial Assignment Framework Created

--- AOCWeek2/AOCWeek2/AOCViewController.m
+++ AOCWeek2/AOCWeek2/AOCViewController.m
@@ -16,6 +16,30 @@
 
 - (void)viewDidLoad
 {
+    //Code Starts Here
+    
+    //Create a UILabel and place it at the top of the view. This is used as the title of the book. Center the text for this label.
+    
+    //Create another label that contains the text "Author:" with the text right justified.
+    
+    //Create a UILabel to the right of the author title label and have it contain the name of the author of the book you chose. This text will be left justified.
+    
+    //Create a UILabel on the next line called "Published:". This text is right justified.
+    
+    //Create a label next to the Published label and add the text of when the book was published. This text is left justified.
+    
+    //Create a UILabel with the text "Summary". This text is left justified.
+    
+    //Create another UILabel that contains a small summary of the book's plot. This text is centered and should span multiple lines.
+    
+    //Create an NSArray of 5 items talked about in the book. These items will be NSStrings. Add the items to the array.
+    
+    //Create a variable of type NSMutableString and allocate it. Loop through the NSArray you created and append each of these items to your NSMutableString separated by commas.
+    
+    //Create a label with the text "List of items" and add it to the parent view. Make sure the text is left justified.
+    
+    //Create another label beneath and set the text to the NSMutableString text. Increase the number of lines if necessary. Make sure the text in this label is centered
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
