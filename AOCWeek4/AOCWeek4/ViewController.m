1a175eadb527baceaa9e7bfb4762c0d108db5fc7
JustinRowe15 <JustinRowe15@fullsail.edu>
2012-12-13 17:56:45 -0500

    2nd Commit For Project 4.

--- AOCWeek4/AOCWeek4/ViewController.m
+++ AOCWeek4/AOCWeek4/ViewController.m
@@ -16,6 +16,20 @@
 
 - (void)viewDidLoad
 {
+    UIButton * loginButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [loginButton setFrame:CGRectMake(200.0f, 70.0f, 90.0f, 30.0f)];
+    [loginButton setTitle:@"Enter" forState:UIControlStateNormal];
+    [self.view addSubview:loginButton];
+    
+    UIButton * dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [dateButton setFrame:CGRectMake(20.0f, 240.0f, 100.0f, 50.0f)];
+    [dateButton setTitle:@"Show Date" forState:UIControlStateNormal];
+    [self.view addSubview:dateButton];
+    
+    UIButton * infoButton = [UIButton buttonWithType:UIButtonTypeInfoDark];
+    [infoButton setFrame: CGRectMake(20.0f, 360.0f, 20.0f, 20.0f)];
+    [self.view addSubview:infoButton];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
