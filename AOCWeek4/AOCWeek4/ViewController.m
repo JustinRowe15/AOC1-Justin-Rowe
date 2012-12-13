0c8cd6e3244969f4e050fdba9e5472d01ee38b15
JustinRowe15 <JustinRowe15@fullsail.edu>
2012-12-13 18:23:55 -0500

    3rd commit for week 4 project.

--- AOCWeek4/AOCWeek4/ViewController.m
+++ AOCWeek4/AOCWeek4/ViewController.m
@@ -16,22 +16,58 @@
 
 - (void)viewDidLoad
 {
+    
+    //User Name Label
+    UILabel * userName = [[UILabel alloc] initWithFrame:CGRectMake(0, 10.0f, 100.0f, 40.0f)];
+    {
+        userName.text = @"Username:";
+        userName.textColor = [UIColor blackColor];
+        userName.textAlignment = NSTextAlignmentLeft;
+    }
+    
+    [self.view addSubview:userName];
+    
+    //Login Field
+    loginField = [[UITextField alloc] initWithFrame:CGRectMake(85.0f, 15.0f, 220.0f, 30.0f)];
+    [loginField setBorderStyle:UITextBorderStyleRoundedRect];
+    [self.view addSubview:loginField];
+    
+    //Login Button
     UIButton * loginButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [loginButton setFrame:CGRectMake(200.0f, 70.0f, 90.0f, 30.0f)];
+    [loginButton setFrame:CGRectMake(215.0f, 50.0f, 90.0f, 30.0f)];
     [loginButton setTitle:@"Enter" forState:UIControlStateNormal];
+    [loginButton addTarget:self action:@selector(loginPress) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:loginButton];
     
+    //Please Enter Username Label
+    UILabel * pleaseEnter = [[UILabel alloc] initWithFrame:CGRectMake(0, 110.0f, self.view.frame.size.width, 70.0f)];
+    {
+        pleaseEnter.text = @"Please Enter Username";
+        pleaseEnter.textColor = [UIColor blueColor];
+        pleaseEnter.textAlignment = NSTextAlignmentCenter;
+        pleaseEnter.backgroundColor = [UIColor lightGrayColor];
+    }
+    
+    [self.view addSubview:pleaseEnter];
+    
+    //Date Button
     UIButton * dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [dateButton setFrame:CGRectMake(20.0f, 240.0f, 100.0f, 50.0f)];
     [dateButton setTitle:@"Show Date" forState:UIControlStateNormal];
     [self.view addSubview:dateButton];
     
+    //Info Button
     UIButton * infoButton = [UIButton buttonWithType:UIButtonTypeInfoDark];
     [infoButton setFrame: CGRectMake(20.0f, 360.0f, 20.0f, 20.0f)];
     [self.view addSubview:infoButton];
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)loginPress
+{
+    NSString * loginText = [loginField text];
 }
 
 - (void)didReceiveMemoryWarning
